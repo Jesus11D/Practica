1976d8064c52c92528a3b6a06f50e4b3026954a9
Jesus <jesusdavidcordova11@gmail.com>
2025-09-04 00:07:01 -0500

    Update codigo.java

--- codigo.java
+++ codigo.java
@@ -1,72 +1,10 @@
-import javax.swing.*;
+import java.io.*;
+import java.util.*;
 
-public class Ordenacion {
-    public static void main(String[] args) {
-        int[] numeros = {5, 3, 8, 4, 2};
+public class ExternalSort {
 
-        JOptionPane.showMessageDialog(null, "Arreglo original: " + arrayToString(numeros));
+    static final int MAX_MEMORIA = 1000;
 
-        int[] burbuja = numeros.clone();
-        burbuja(burbuja);
-        JOptionPane.showMessageDialog(null, "Burbuja: " + arrayToString(burbuja));
-
-        int[] seleccion = numeros.clone();
-        seleccion(seleccion);
-        JOptionPane.showMessageDialog(null, "Selección: " + arrayToString(seleccion));
-
-        int[] insercion = numeros.clone();
-        insercion(insercion);
-        JOptionPane.showMessageDialog(null, "Inserción: " + arrayToString(insercion));
-    }
-
-    public static void burbuja(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-    public static void seleccion(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            int min = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[min]) {
-                    min = j;
-                }
-            }
-            int temp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = temp;
-        }
-    }
-
-    public static void insercion(int[] arr) {
-        int n = arr.length;
-        for (int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = key;
-        }
-    }
-
-    public static String arrayToString(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        for (int n : arr) {
-            sb.append(n).append(" ");
-        }
-        return sb.toString();
-    }
-}
-
-p
+    public static void main(String[] args) throws IOException {
+        String inputFile = "entrada.txt";
+        String outputFile = "salida_ordenada.txt";
